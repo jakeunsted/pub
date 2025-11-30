a0f00c55e2c66bb8af49dddb86e15fdccc2929fd
Jake Unsted <jake.unsted@gmail.com>
2025-11-30 13:21:35 +0000

    setup superbase db and types

--- lib/superbase.ts
+++ lib/superbase.ts
@@ -1,3 +1,4 @@
+import { Database } from '@/types/database.types';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 import 'react-native-url-polyfill/auto';
@@ -5,7 +6,7 @@ import 'react-native-url-polyfill/auto';
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,
